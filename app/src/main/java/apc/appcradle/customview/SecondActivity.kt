df5fb3fba9f1fdcf4ec40ff5cbc9e428c0c5b07b
Sergey Boykov <sutemi67@gmail.com>
2025-01-07 11:34:55 +0300

    Initial commit

---
+++ app/src/main/java/apc/appcradle/customview/SecondActivity.kt
@@ -0,0 +1,23 @@
+package apc.appcradle.customview
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_second)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.second)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        findViewById<Button>(R.id.top_button).text = "Top button - Second Activity"
+        findViewById<Button>(R.id.bottom_button).text = "Bottom button - Second Activity"
+    }
+}
